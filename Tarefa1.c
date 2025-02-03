0c7a0ead0f765b60bce59fc76cf746673cfbd1de
Joel Santos <joeltere9@gmail.com>
2025-02-03 09:59:16 -0300

    change in the simulator

--- Tarefa1.c
+++ Tarefa1.c
@@ -5,10 +5,42 @@ const uint LED_VERMELHO = 11;
 const uint LED_AMARELO = 12;
 const uint LED_VERDE = 13;
 
+typedef enum {
+    VERMELHO,
+    AMARELO,
+    VERDE
+} EstadoSemaforo;
+
+volatile EstadoSemaforo estado_atual = VERMELHO;
+
+bool repeating_timer_callback(struct repeating_timer *t) {
+    gpio_put(LED_VERMELHO, 0);
+    gpio_put(LED_AMARELO, 0);
+    gpio_put(LED_VERDE, 0);
+    
+    switch(estado_atual) {
+        case VERMELHO:
+            estado_atual = AMARELO;
+            gpio_put(LED_AMARELO, 1);
+            printf("Mudança: AMARELO\n");
+            break;
+        case AMARELO:
+            estado_atual = VERDE;
+            gpio_put(LED_VERDE, 1);
+            printf("Mudança: VERDE\n");
+            break;
+        case VERDE:
+            estado_atual = VERMELHO;
+            gpio_put(LED_VERMELHO, 1);
+            printf("Mudança: VERMELHO\n");
+            break;
+    }
+    return true;
+}
+
 int main() {
     stdio_init_all();
-    sleep_ms(1000);
-
+    
     gpio_init(LED_VERMELHO);
     gpio_init(LED_AMARELO);
     gpio_init(LED_VERDE);
@@ -16,21 +48,14 @@ int main() {
     gpio_set_dir(LED_VERMELHO, GPIO_OUT);
     gpio_set_dir(LED_AMARELO, GPIO_OUT);
     gpio_set_dir(LED_VERDE, GPIO_OUT);
-
-    while (true) {
-        gpio_put(LED_VERMELHO, 1);
-        printf("Vermelho\n");
-        sleep_ms(3000);
-        gpio_put(LED_VERMELHO, 0);
-
-        gpio_put(LED_AMARELO, 1);
-        printf("Amarelo\n");
-        sleep_ms(3000);
-        gpio_put(LED_AMARELO, 0);
-
-        gpio_put(LED_VERDE, 1);
-        printf("Verde\n");
-        sleep_ms(3000);
-        gpio_put(LED_VERDE, 0);
+    
+    gpio_put(LED_VERMELHO, 1);
+    
+    struct repeating_timer timer;
+    add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer);
+    
+    while(1) {
+        printf("Sistema em execução - Estado atual: %d\n", estado_atual);
+        sleep_ms(1000);
     }
 }
